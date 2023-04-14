12897290244cacc6d76716f9d557585b32744fea
unknown <shirleyapril23@gmail.com>
2023-04-14 16:09:39 +0300

    creating dashboard

--- components/Owner/Products/UploadProducts/index.jsx
+++ components/Owner/Products/UploadProducts/index.jsx
@@ -1,7 +1,27 @@
-import { Typography } from "@mui/material";
+import { Dialog, Stack, Typography, Button } from "@mui/material";
+import { useState } from "react";
 
 const UploadProducts = () => {
-  return <Typography>Form to upload UploadProducts</Typography>;
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Stack>
+      <Stack justifyContent="flex-end" alignItems="flex-end" sx={{m: 2, pr: 5}}>
+        <Button variant="contained" onClick={handleOpen}>New Product</Button>
+      </Stack>
+      <Dialog open={open} onClose={handleClose}>
+        <Typography>Form Values in here</Typography>
+      </Dialog>
+    </Stack>
+  );
 };
 
 export default UploadProducts;
